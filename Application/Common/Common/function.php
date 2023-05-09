fd804db1de6f9034c303719228725e701f5bd170
15239136251 <2532281393@qq.com>
2023-05-09 17:39:09 +0800

    🐞 fix(修复了少传参数报错的问题):

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -1,6 +1,6 @@
 <?php
 // 自定义code || msg || data 返回
-function json_out($code, $msg, $data) {
+function json_out($code = -1, $msg = "请求失败!", $data) {
     $result = array(
         "code" => $code,
         "msg" => $msg,
@@ -11,20 +11,20 @@ function json_out($code, $msg, $data) {
 }
 
 // 返回错误结果
-function resp_error($code, $msg) {
+function resp_error($code = -1, $msg = "请求失败!") {
     $result = array(
-        "code" => $code ? $code : -1,
-        "msg" => $msg ? $msg : '出错了!'
+        "code" => $code,
+        "msg" => $msg
     );
     echo json_encode($result);
     exit;
 }
 
 // 返回成功结果
-function resp_success($data) {
+function resp_success($data, $msg = "请求成功!") {
     $result = array(
         "code" => 100,
-        "msg" => "请求成功!",
+        "msg" => $msg ,
         "data" => $data
     );
     echo json_encode($result);
