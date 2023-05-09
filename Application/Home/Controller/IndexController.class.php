2d9605fae5e5e5a62105686174d915357ab675b1
15239136251 <2532281393@qq.com>
2023-05-09 17:40:57 +0800

    ✨ feat(添加登录方法):

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -1,30 +1,86 @@
 <?php
 namespace Home\Controller;
-use Think\Controller\RestController;
-class IndexController extends RestController {
-	
-	protected $allowMethod    = array('get','post','put'); // REST允许的请求类型列表
-    protected $allowType      = array('html','xml','json'); // REST允许请求的资源类型列表
+use Home\Controller\CommonController;
 
-    Public function index(){
-        // 输出id为1的Info的html页面
-		$input=file_get_contents('php://input');
-		//dump(json_decode($input,true));
-		$arr =  D('myguests')->select();
-		$this->response(json_out(100, "请求成功!", $arr));
+class IndexController extends CommonController {
+
+	function _initialize() {
+        parent::_initialize();
     }
 
-	public function hello() {
-		$this->response('');
+    public function index() {
+		
 	}
 
-	public function getParams() {
-
-		$params['value'] = I("value");
-		$params['name'] = I("name");
-		$this->response(resp_success($params));
-		return $params;
+	public function getCode() {
+		$result = $this->checkToken();
+		resp_success($result);
 	}
-   
+
+	// 检测输入的验证码是否正确，$code为用户输入的验证码字符串
+	function check_verify($code, $id = ''){
+		$verify = new \Think\Verify();
+		return $verify->check($code, $id);
+	}
+
+	public function login() {
+		$params['username'] = I("username");
+		$params['password'] = I("password");
+		$params['code'] = I("code");
+
+		if (!$params['username']) {
+			resp_error(0, "未输入账号!");
+		}
+
+		if (!$params['password']) {
+			resp_error(0, "未输入密码!");
+		}
+
+		if (!$params['code']) {
+			resp_error(0, "未输入验证码!");
+		}
+
+		$where['is_active'] ="Y";
+		$where['username'] = $params['username'];
+		$data = $this->user->getList($where);
+		
+		if (count($data)) {
+			// 有用户，判断密码是否一致
+			if ($data[0]['password'] === $params['password']) {
+				// $userinfo = $this->user->getUserInfo($data[0]['id']);
+
+				$userinfo = [
+					'id' => $data[0]['id'], 
+					'name' => $data[0]['username']
+				];
+
+				$jwt = new \Common\Utils\Jwt();
+				$jwt->setIss("{$data[0]['username']}");//JWT的签发者
+				$jwt->setIat(time());//签发时间
+				$jwt->setExp(time() + 60 * 60 * 24 * 1);//过期时间为1天
+				$jwt->setClaim($userinfo);//存储数据
+				//生成token并获取
+				$token = $jwt->getToken();
+
+				$result = array(
+					'userinfo'=> $userinfo,
+					'token'=> $token
+				);
+				resp_success($result, "登录成功!");
+			} else {
+				resp_error(0, "密码错误!");
+			}
+		} else {
+			resp_error(0, '当前用户不存在，无法登录!');
+		}
+	}
+
+	public function logout() {
+
+	}
+
+	private function _filterKey($val) {
+		return $val == 'id';
+	}
 }
 
