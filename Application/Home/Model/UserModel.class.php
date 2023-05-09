0492df5dc5f41ffc6f1ea943ceb56d531bcf309e
15239136251 <2532281393@qq.com>
2023-05-09 17:40:19 +0800

    🦄 refactor(使用通用模型):

--- Application/Home/Model/UserModel.class.php
+++ Application/Home/Model/UserModel.class.php
@@ -1,22 +1,12 @@
 <?php
 namespace Home\Model;
 use Think\Model;
-class UserModel extends Model{
+
+use Home\Model\CommonModel;
+class UserModel extends CommonModel{
     protected $tableName = 'user';
 
-    function getList($where = "") {
-        $data = $this->where("is_active = 'Y'".$where)->select();
-        return $data;
-    }
-
-    function getDetail($id) {
-        $data = $this->where("id = {$id} AND is_active = 'Y'")->find();
-        return $data;
-    }
-
-    function add($params) {
-        $this->create($params);
-        $lastId = $this->add();
-        return $lastId;
+    public function getUserInfo($id) {
+        return $id;
     }
 }
